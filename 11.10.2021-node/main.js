49a630019d3dba1161c7ef1c7c803d2c9ea2bbd9
segedu <seggedu20@gmail.com>
2021-10-11 17:00:58 +0300

    first commit

---
+++ 11.10.2021-node/main.js
@@ -0,0 +1,25 @@
+//! command_line_1
+
+//! command_line_2
+
+//! command_line_3
+
+//!file_sync_1
+
+//!file_sync_2
+
+//!file_sync_3
+
+//!file_sync_4
+
+//!file_sync_5
+
+//!array_operation_1
+
+//!array_operation_2
+
+//!array_operation_3
+
+//!array_operation_4
+
+//!array_operation_5
