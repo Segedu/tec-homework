606d87f75440bab91c12ae34fe24a37984f29007
segedu <seggedu20@gmail.com>
2021-10-18 11:12:23 +0300

    11.10.2021

--- 11.10.2021-node/main.js
+++ 11.10.2021-node/main.js
@@ -1,7 +1,27 @@
 //! command_line_1
+// let numsArray = [1, 2, 3, 4, 5, 6];
+// let newNum = process.argv[2];
+// numsArray.findIndex(findInArrayGivesIndex);
+
+// function findInArrayGivesIndex(newNum) {
+//   return newNum >= newNum;
+// }
 
 //! command_line_2
 
+const numArray = [2, 7, 4, 9, 6];
+const someString = process.argv[2];
+function findMax() {
+  let max = 0;
+  for (let i = 0; i < numArray.length; i++) {
+    if (numArray[i] > max && someString == "max") {
+      max = numArray[i];
+      console.log(` The max element: ${max}`);
+    }
+  }
+  return max;
+}
+findMax();
 //! command_line_3
 
 //!file_sync_1
