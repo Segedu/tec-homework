94451a0ff99657f477c1e49cc2fcb6c3d1f46076
segedu <seggedu20@gmail.com>
2021-10-21 18:30:04 +0300

    21.10.2021

--- 19.10.2021- express project/app.js
+++ 19.10.2021- express project/app.js
@@ -2,34 +2,51 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const port = 8080;
-const publicPath = path.join(__dirname, "public");
+// const publicPath = path.join(__dirname, "public" "views", "layouts");
+const handlebars = require("express-handlebars");
+const { send } = require("process");
+let cities, lon, lat;
 
-app.use(express.static(publicPath));
+app.use(express.static("public"));
+app.set("view engine", "hbs");
+app.engine(
+  "hbs",
+  handlebars({ layoutsDir: `${__dirname}/views/layouts`, extname: "hbs" })
+);
+
 app.get("/", (req, res) => {
-  res.sendFile("index.html");
+  res.render("main", { layout: "index" });
+});
+
+app.get("/findLocation.hbs", (req, res) => {
+  res.render("main", { layout: "findLocation" });
 });
 
 app.get("/city", (req, res) => {
-  const cityName = `${req.query.city}`;
   const APIkey = "641974375fb31ab9962b7f26bce1a2a0";
-  const url = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIkey}`;
+  const cityName = `${req.query.city}`;
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIkey}`;
+  const axios = require("axios");
 
-  axios
+  return axios
     .get(url)
-    .then(function (res) {
-      console.log("success");
-      console.log(res);
-      if (res.status === 200) {
-        console.log(res.data);
+    .then(function (response) {
+      if (response.status === 200) {
+        cities = JSON.stringify(req.query.city);
+        lon = JSON.stringify(response.data.coord.lon);
+        lat = JSON.stringify(response.data.coord.lat);
+        res.render("main", { layout: "findLocation", lon, lat });
       }
     })
     .catch(function (error) {
+      res.sendFile(path.join(__dirname, "public", "error.html"));
+      console.log("i am in catch");
       console.log("error");
-      console.log(error);
     });
-  res.send(`${req.query.city}`);
-  // res.send(`longitude: ${req.query.longitude}, latitude:${req.query.latitude}`);
-  // res.sendFile(publicPath, "findLocation.html");
+});
+
+app.get("*", (req, res) => {
+  res.send("sorry your page can't be reached!!");
 });
 
 app.listen(port);
