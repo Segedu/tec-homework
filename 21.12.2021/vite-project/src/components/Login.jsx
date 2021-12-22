5fcbacce2ba734b95c94a3631a37a28a38e2b89b
Segedu <seggedu20@gmail.com>
2021-12-22 17:20:19 +0200

    21.12.2021

--- 21.12.2021/vite-project/src/components/Login.jsx
+++ 21.12.2021/vite-project/src/components/Login.jsx
@@ -1,23 +1,24 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
 
 const Login = () => {
-    const userName = useRef();
-    const userEmail = useRef();
-    const userPassword = useRef()
+    const userName = useRef(), userEmail = useRef(), userPassword = useRef();
+    const [disabled, setDisabled] = useState(true);
 
+    const isValid = () => {
+        return userName.current.value && userEmail.current.value && userPassword.current.value !== ""
+    }
 
     return (
-        <form onSubmit={
-            (e) => {
-                e.preventDefault()
-                if (userName.current.value && userEmail.current.value && userPassword.current.value !== "") {
-                    console.log(userName.current.value, userEmail.current.value, userPassword.current.value)
-                }
-            }}>
+        <form onChange={() => { setDisabled(isValid) }} onSubmit={(e) => {
+            e.preventDefault()
+            if (isValid) {
+                console.log(userName.current.value, userEmail.current.value, userPassword.current.value)
+            }
+        }}>
             <input ref={userName} type="text" placeholder="Enter Name" /><br />
             <input ref={userEmail} type="email" placeholder="Enter Email" /><br />
             <input ref={userPassword} type="password" placeholder="Enter your password" />
-            <input type="submit" value="Log In" />
+            <input type="submit" value="Log In" disabled={!disabled} />
         </form>)
 }
 
